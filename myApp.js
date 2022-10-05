3d7fe7205d9fdbf80f19cc377394505090fb811b
neeraj <neerajp4321@gmail.com>
2022-10-05 12:57:57 +0530

    Feat: get userinput another way

--- myApp.js
+++ myApp.js
@@ -38,6 +38,9 @@ app.get('/:word/echo', (req, res) => {
   res.json({ "echo": req.params.word })
 })
 
+app.route('/name').get((req, res) => {
+  res.json({ "name": `${req.query.first} ${req.query.last}` })
+})
 
 
 
