6ffb321c85dabee37a79272f98c53078506e4313
neeraj <neerajp4321@gmail.com>
2022-10-05 12:35:26 +0530

    Feat: add middleware chain

--- myApp.js
+++ myApp.js
@@ -27,6 +27,12 @@ app.get('/json', (req, res) => {
   res.json({ "message": "Hello json" })
 })
 
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString()
+  next()
+}, (req, res) => {
+  res.json({ "time": req.time })
+})
 
 
 
